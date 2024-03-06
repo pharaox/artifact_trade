6dbebd35a677e97346c036de3769be2a47c1b5a6
pharaox <pharaox@tutanota.com>
2024-03-06 22:45:30 +0200

    Use a variable for the log file in analyze.sh

--- misc/analyze.sh
+++ misc/analyze.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+LOGFILE=debug.log
+
 # Artifact events
-echo "Artifact bought: $(grep -a "Artifact bought" debug.log | wc -l)"
-echo "Artifact destroyed: $(grep -a "Artifact destroyed" debug.log | wc -l)"
-echo "- by Aniconist ruler: $(grep -a "Artifact destroyed by Aniconist ruler" debug.log | wc -l)"
+echo "Artifact bought: $(grep -a "Artifact bought" $LOGFILE | wc -l)"
+echo "Artifact destroyed: $(grep -a "Artifact destroyed" $LOGFILE | wc -l)"
+echo "- by Aniconist ruler: $(grep -a "Artifact destroyed by Aniconist ruler" $LOGFILE | wc -l)"
 echo
