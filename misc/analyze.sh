f6f2e1c5967899ebe032199f662614717355cf8e
pharaox <pharaox@tutanota.com>
2024-10-26 11:56:35 +0300

    Enhance debug logging

--- misc/analyze.sh
+++ misc/analyze.sh
@@ -3,7 +3,12 @@
 LOGFILE=debug.log
 
 # Artifact events
-echo "Artifact bought: $(grep -a "Artifact bought" $LOGFILE | grep -v "AGOT" | wc -l)"
-echo "Artifact destroyed: $(grep -a "Artifact destroyed" $LOGFILE | grep -v "AGOT" | wc -l)"
-echo "- by Aniconist ruler: $(grep -a "Artifact destroyed by Aniconist ruler" $LOGFILE | wc -l)"
+echo "Artifact bought: $(grep -a "Artifact bought," $LOGFILE | grep -v "AGOT" | wc -l)"
+echo "- weaponsmith: $(grep -a "Artifact sold,.*weaponsmith" $LOGFILE| wc -l)"
+echo "- armorer: $(grep -a "Artifact sold,.*armorer" $LOGFILE | wc -l)"
+echo "- jeweler: $(grep -a "Artifact sold,.*jeweler" $LOGFILE | wc -l)"
+echo "Artifact sold: $(grep -a "Artifact sold," $LOGFILE | grep -v "AGOT" | wc -l)"
+echo "- peddler: $(grep -a "Artifact sold,.*peddler" $LOGFILE | wc -l)"
+echo "Artifact destroyed: $(grep -a "Artifact destroyed," $LOGFILE | grep -v "AGOT" | wc -l)"
+echo "- aniconist: $(grep -a "Artifact destroyed,.*aniconist: yes" $LOGFILE | wc -l)"
 echo
