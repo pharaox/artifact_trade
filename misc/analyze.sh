a6cffc194431de124c4c6196f16f1bdb984e7824
pharaox <pharaox@tutanota.com>
2024-10-26 16:35:32 +0300

    Improve decision events AI chances and cleanup logic

--- misc/analyze.sh
+++ misc/analyze.sh
@@ -4,9 +4,9 @@ LOGFILE=debug.log
 
 # Artifact events
 echo "Artifact bought: $(grep -a "Artifact bought," $LOGFILE | grep -v "AGOT" | wc -l)"
-echo "- weaponsmith: $(grep -a "Artifact sold,.*weaponsmith" $LOGFILE| wc -l)"
-echo "- armorer: $(grep -a "Artifact sold,.*armorer" $LOGFILE | wc -l)"
-echo "- jeweler: $(grep -a "Artifact sold,.*jeweler" $LOGFILE | wc -l)"
+echo "- weaponsmith: $(grep -a "Artifact bought,.*weaponsmith" $LOGFILE| wc -l)"
+echo "- armorer: $(grep -a "Artifact bought,.*armorer" $LOGFILE | wc -l)"
+echo "- jeweler: $(grep -a "Artifact bought,.*jeweler" $LOGFILE | wc -l)"
 echo "Artifact sold: $(grep -a "Artifact sold," $LOGFILE | grep -v "AGOT" | wc -l)"
 echo "- peddler: $(grep -a "Artifact sold,.*peddler" $LOGFILE | wc -l)"
 echo "Artifact destroyed: $(grep -a "Artifact destroyed," $LOGFILE | grep -v "AGOT" | wc -l)"
